d28227f968532b0f9c707f461fa895727090e76f
Simon Erkelens <simonerkelens@silverstripe.com>
2017-06-21 12:15:02 +1200

    Make the UTM links work

--- code/extensions/GoogleShortenerExtension.php
+++ code/extensions/GoogleShortenerExtension.php
@@ -4,15 +4,22 @@
 class GoogleShortenerExtension extends DataExtension
 {
 
-    protected static $social_media = [
-        'TW',
-        'FB'
-    ];
+    /**
+     * @var GoogleShortenerService
+     */
+    protected $service;
 
-    protected static $required_utm = [
-        'UTMSource',
-        'UTMMedium',
-        'UTMCampaign',
+    private static $db = [
+        'FBUTMSource'   => 'Varchar(255)',
+        'FBUTMMedium'   => 'Varchar(255)',
+        'FBUTMCampaign' => 'Varchar(255)',
+        'FBUTMTerm'     => 'Varchar(255)',
+        'FBUTMContent'  => 'Varchar(255)',
+        'TWUTMSource'   => 'Varchar(255)',
+        'TWUTMMedium'   => 'Varchar(255)',
+        'TWUTMCampaign' => 'Varchar(255)',
+        'TWUTMTerm'     => 'Varchar(255)',
+        'TWUTMContent'  => 'Varchar(255)',
     ];
 
     protected static $utm_to_url = [
@@ -32,18 +39,20 @@ class GoogleShortenerExtension extends DataExtension
      */
     public function onBeforeWrite()
     {
+        parent::onBeforeWrite();
+
         if ($this->owner->destroy) {
             $this->owner->ShortURL()->delete();
             $this->owner->ShortURLID = 0;
         }
-
-        $utmData = SiteConfig::current_site_config(); // @todo
 
         $apiKey = GoogleShortenerService::config()->get('googlapi');
         $shortURL = ShortURL::get()->filter(['ID' => $this->owner->ShortURLID])->first();
 
         if ($apiKey && (!$shortURL || !$shortURL->exists())) {
             $shortURL = ShortURL::create();
+            $id = $shortURL->write();
+            $this->owner->ShortURLID = $id;
 
             $longUrl = $this->owner->AbsoluteLink();
 
@@ -51,19 +60,41 @@ class GoogleShortenerExtension extends DataExtension
                 'longUrl' => $longUrl,
             );
 
-            $service = Injector::inst()->get('GoogleShortenerService');
+            $this->service = Injector::inst()->get('GoogleShortenerService');
 
-            $result = $service->getShortLink($postData);
+            $result = $this->service->getShortLink($postData);
 
             // Always get a standard short URL
             if ($result && $result->id) {
                 $shortURL->ShortURL = $result->id;
-                $id = $shortURL->write();
-                $this->owner->ShortURLID = $id;
+            }
+            $this->getUTMLinks($shortURL);
+
+            $shortURL->write();
+        }
+
+    }
+
+    public function getUTMLinks($shortURL)
+    {
+        $owner = $this->owner;
+        $mediumString = [];
+        foreach (['FB', 'TW'] as $medium) {
+            if ($owner->$medium . 'UTMSource' && $owner->$medium . 'UTMMedium' && $owner->$medium . 'UTMCampaign') {
+                foreach (static::$utm_to_url as $key => $value) {
+                    $field = $medium . $key;
+                    $mediumString[] = $value . '=' . $owner->{$field};
+                }
+
+                $result = $this->service->getShortLink([
+                    'longUrl' => $owner->AbsoluteLink() . '?' . implode('&', $mediumString)
+                ]);
+                if ($result && $result->id) {
+                    $shortURL->{$medium . 'ShortURL'} = $result->id;
+                    $shortURL->write();
+                }
             }
         }
-        parent::onBeforeWrite();
-
     }
 
     public function updateCMSFields(FieldList $fields)
@@ -71,6 +102,23 @@ class GoogleShortenerExtension extends DataExtension
         parent::updateCMSFields($fields);
         $fields->removeByName(['ShortURL']);
         $shortURL = ShortURL::get()->filter(['ID' => $this->owner->ShortURLID])->first();
+        $fields->addFieldsToTab(
+            'Root.ShareLinks',
+            [
+                HeaderField::create('FBUTM', 'Facebook UTM parameters'),
+                $FBUTMSource = TextField::create('FBUTMSource', 'UTM Source for Facebook'),
+                $FBUTMMedium = TextField::create('FBUTMMedium', 'UTM Medium for Facebook'),
+                $FBUTMCampaign = TextField::create('FBUTMCampaign', 'UTM Campaign for Facebook'),
+                $FBUTMTerm = TextField::create('FBUTMTerm', 'UTM Term for Facebook'),
+                $FBUTMContent = TextField::create('FBUTMContent', 'UTM Content for facebook'),
+                HeaderField::create('TWUTM', 'Twitter UTM parameters'),
+                $TWUTMSource = TextField::create('TWUTMSource', 'UTM Source for Twitter'),
+                $TWUTMMedium = TextField::create('TWUTMMedium', 'UTM Medium for Twitter'),
+                $TWUTMCampaign = TextField::create('TWUTMCampaign', 'UTM Campaign for Twitter'),
+                $TWUTMTerm = TextField::create('TWUTMTerm', 'UTM Term for Twitter'),
+                $TWUTMContent = TextField::create('TWUTMContent', 'UTM Content for Twitter'),
+            ]
+        );
         if ($shortURL && $shortURL->exists()) {
             $fields->addFieldsToTab(
                 'Root.ShareLinks',
@@ -92,19 +140,23 @@ class GoogleShortenerExtension extends DataExtension
                         '<p>Copy the URL and add ".info" at the end to see the usage statistics'
                     ),
                     CheckboxField::create('destroy', 'Reset the short URL\'s')
-                ]
+                ],
+                'FBUTM'
             );
         }
+
+        $FBUTMSource->setDescription('Required parameter to identify the source of your traffic such as: search engine, newsletter, or other referral.');
+        $FBUTMMedium->setDescription('Required parameter to identify the medium the link was used upon such as: email, CPC, or other method of sharing.');
+        $FBUTMCampaign->setDescription('Required parameter to identify a specific product promotion or strategic campaign such as a spring sale or other promotion.');
+        $FBUTMTerm->setDescription('Optional parameter suggested for paid search to identify keywords for your ad. You can skip this for Google AdWords if you have connected your AdWords and Analytics accounts and use the auto-tagging feature instead.');
+        $FBUTMContent->setDescription('Optional parameter for additional details for A/B testing and content-targeted ads.');
+
+        $TWUTMSource->setDescription('Required parameter to identify the source of your traffic such as: search engine, newsletter, or other referral.');
+        $TWUTMMedium->setDescription('Required parameter to identify the medium the link was used upon such as: email, CPC, or other method of sharing.');
+        $TWUTMCampaign->setDescription('Required parameter to identify a specific product promotion or strategic campaign such as a spring sale or other promotion.');
+        $TWUTMTerm->setDescription('Optional parameter suggested for paid search to identify keywords for your ad. You can skip this for Google AdWords if you have connected your AdWords and Analytics accounts and use the auto-tagging feature instead.');
+        $TWUTMContent->setDescription('Optional parameter for additional details for A/B testing and content-targeted ads.');
+
     }
 
 }
-
-/*
- *
-    Campaign Source (utm_source) – Required parameter to identify the source of your traffic such as: search engine, newsletter, or other referral.
-    Campaign Medium (utm_medium) – Required parameter to identify the medium the link was used upon such as: email, CPC, or other method of sharing.
-    Campaign Term (utm_term) – Optional parameter suggested for paid search to identify keywords for your ad. You can skip this for Google AdWords if you have connected your AdWords and Analytics accounts and use the auto-tagging feature instead.
-    Campaign Content (utm_content) – Optional parameter for additional details for A/B testing and content-targeted ads.
-    Campaign Name (utm_campaign) – Required parameter to identify a specific product promotion or strategic campaign such as a spring sale or other promotion.
-
- */
